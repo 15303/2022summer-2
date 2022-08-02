91f4dc342d53d9b6d561bd0470b5a52e2a929a06
Max613Code <bzhouadult@gmail.com>
2022-08-02 10:55:24 -0700

    Changed more controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/max/MaxTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/max/MaxTeleOp.java
@@ -8,30 +8,64 @@ import org.firstinspires.ftc.teamcode.Robot;
 @TeleOp(name="MaxTeleOp")
 public class MaxTeleOp extends LinearOpMode {
     private Robot robot; //store the robot
+    private boolean lockGrabber = false;
+    private float driveMultFactor = 1;
 
     public void runOpMode() {
         robot = new Robot(this); //create a new Robot
 
         waitForStart(); //required
 
-        //so we want something that runs coninuously we do that using a while loop
+        //so we want something that runs continuously we do that using a while loop
         while (opModeIsActive()) {
             //normally gamepad1.right_stick_y is reversed, so we make it negative
-            double drive = -gamepad1.right_stick_y * 0.6;
-            double turn = gamepad1.right_stick_x * 0.6;
+            double drive = -gamepad1.right_stick_y * 0.6 * driveMultFactor;
+            double turn = gamepad1.right_stick_x * 0.6 * driveMultFactor;
             robot.driveComponent(drive, turn); //power the drive motors given the joystick controls
 
-            robot.aim(gamepad1.left_stick_x * 0.5);
+            if (abs(-gamepad1.left_stick_y) < 0.4) {
+                robot.aim(gamepad1.left_stick_x * 0.2);
+            }
             robot.lift(-gamepad1.left_stick_y * 0.5);
             robot.spinCarousel(gamepad1.right_trigger - gamepad1.left_trigger);
+
+            if (gamepad1.dpad_down) { //Dpad logic to turn on grabber locks, precision mode
+                lockGrabber = true;
+            } else if (gamepad1.dpad_up) {
+                lockGrabber = false;
+            }
+            if (gamepad1.dpad_left) {
+                driveMultFactor = 0.3f;
+            } else if (gamepad1.dpad_right) {
+                driveMultFactor = 1;
+            }
 
             if (gamepad1.a) {
                 robot.grab(1);
             } else if (gamepad1.b) {
                 robot.grab(-1);
-            } else {
+            } else if (!lockGrabber){
                 robot.grab(0);
             }
+
+
+            //Controls:
+            /* Right stick y drives the robot, up = forwards, left and right turn
+            Left stick turns the arm, left and right turn the arm if not currently lifting it,
+            up and down move the arm as expected
+            A should grab, B should release by controlling the servo
+            Dpad down locks the grabber so it keeps rotating as currently is, Dpad up negates this
+            Dpad left turns on precision driving mode, making driving 0.3 times slower, Dpad right negates thsi
+            Carousel driven by the big buttons on the top of the controller.
+             */
+        }
+    }
+
+    public float abs(float num) {
+        if (num >= 0) {
+            return num;
+        } else {
+            return -num;
         }
     }
 

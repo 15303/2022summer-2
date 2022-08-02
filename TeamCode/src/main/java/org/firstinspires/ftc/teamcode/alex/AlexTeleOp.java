de860e41aad02ee616ef8d60c203d7da387330d7
Clincot <courtneyg509@gmail.com>
2022-08-02 11:06:23 -0700

    add AlexTeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/alex/AlexTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/alex/AlexTeleOp.java
@@ -14,24 +14,26 @@ public class AlexTeleOp extends LinearOpMode {
 
         waitForStart(); //required
 
-        //so we want something that runs coninuously we do that using a while loop
+        //so we want something that runs continuously we do that using a while loop
         while (opModeIsActive()) {
             //normally gamepad1.right_stick_y is reversed, so we make it negative
             double drive = -gamepad1.right_stick_y * 0.5;
             double turn = gamepad1.right_stick_x * 0.5;
-            robot.driveComponent(drive, turn); //power the drive motors given the joystick controls
+            double driveSlow = -gamepad1.left_stick_y * 0.25;
+            double turnSlow = gamepad1.left_stick_x * 0.25;
+            robot.driveComponent(drive + driveSlow, turn + turnSlow); //power the drive motors given the joystick controls
 
             float left_stick_x = gamepad2.left_stick_x;
-            robot.aim( left_stick_x * 0.5);
+            robot.aim( left_stick_x * 0.4);
             float left_stick_y = gamepad2.left_stick_y;
-            robot.lift(left_stick_y * 0.5);
+            robot.lift(left_stick_y * 0.4);
             robot.spinCarousel(gamepad2.right_trigger - gamepad2.left_trigger);
 
             if (gamepad2.a) {
                 robot.grab(1);
             } else if (gamepad2.b) {
                 robot.grab(-1);
-            } else {
+            } else if (gamepad2.right_bumper) {
                 robot.grab(0);
             }
         }

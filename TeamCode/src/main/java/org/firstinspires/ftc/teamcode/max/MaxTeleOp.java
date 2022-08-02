df7df48b20f21b99bd09e85a5c5e7a6788278249
Max613Code <bzhouadult@gmail.com>
2022-08-02 09:59:41 -0700

    Changed input sensitivity

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/max/MaxTeleOp.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.max;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Robot;
+
+@TeleOp(name="MaxTeleOp")
+public class MaxTeleOp extends LinearOpMode {
+    private Robot robot; //store the robot
+
+    public void runOpMode() {
+        robot = new Robot(this); //create a new Robot
+
+        waitForStart(); //required
+
+        //so we want something that runs coninuously we do that using a while loop
+        while (opModeIsActive()) {
+            //normally gamepad1.right_stick_y is reversed, so we make it negative
+            double drive = -gamepad1.right_stick_y * 0.7;
+            double turn = gamepad1.right_stick_x * 0.7;
+            robot.driveComponent(drive, turn); //power the drive motors given the joystick controls
+
+            robot.aim(gamepad2.left_stick_x * 0.5);
+            robot.lift(-gamepad2.left_stick_y * 0.5);
+            robot.spinCarousel(gamepad2.right_trigger - gamepad2.left_trigger);
+
+            if (gamepad2.a) {
+                robot.grab(1);
+            } else if (gamepad2.b) {
+                robot.grab(-1);
+            } else {
+                robot.grab(0);
+            }
+        }
+    }
+
+}

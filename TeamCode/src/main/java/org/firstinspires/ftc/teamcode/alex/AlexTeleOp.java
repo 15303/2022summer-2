8b14ac3bf97d4abf41a165478c824e536a7158e2
Clincot <courtneyg509@gmail.com>
2022-08-02 11:22:15 -0700

    add AlexTeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/alex/AlexTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/alex/AlexTeleOp.java
@@ -26,7 +26,7 @@ public class AlexTeleOp extends LinearOpMode {
             float left_stick_x = gamepad2.left_stick_x;
             robot.aim( left_stick_x * 0.3);
             float left_stick_y = gamepad2.left_stick_y;
-            robot.lift(left_stick_y * 0.3);
+            robot.lift(left_stick_y * 0.4);
             robot.spinCarousel(gamepad2.right_trigger - gamepad2.left_trigger);
 
             if (gamepad2.a) {

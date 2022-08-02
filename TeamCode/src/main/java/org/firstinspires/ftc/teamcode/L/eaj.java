97e90e1293f09ce9d3d8506276b0e738e3e9a0cc
jinfinity102 <kevinttan05@gmail.com>
2022-08-02 10:04:52 -0700

    kevin tan attempts java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/L/eaj.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/L/eaj.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Robot;
 
 package org.firstinspires.ftc.teamcode;
-
+//hi
         import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
         import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 

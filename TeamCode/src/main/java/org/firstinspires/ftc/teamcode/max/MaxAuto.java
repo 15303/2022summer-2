e9f7e3e6400d3bf6d5347eec3fc3139ead87139b
Max613Code <bzhouadult@gmail.com>
2022-08-03 10:58:28 -0700

    Changed turning timing another time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/max/MaxAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/max/MaxAuto.java
@@ -32,7 +32,11 @@ private Robot robot;
         sleep(4000);
         robot.grabber.setPower(0);
 
-        sleep(4000); //Transition
+        sleep(2000); //Transition
+
+        robot.lifter.setPower(0.3); //move lifter to not interfere with shuttle hub
+        sleep(100);
+        robot.lifter.setPower(0);
 
         robot.drive(-0.35);//Turning and driving to be orientated toward the coursasel
         sleep(800);
@@ -43,7 +47,7 @@ private Robot robot;
 
         //robot.turnDegrees(90);
         robot.turn(-0.5);
-        sleep(900);
+        sleep(320);
 
         sleep(4000); //Transition
 

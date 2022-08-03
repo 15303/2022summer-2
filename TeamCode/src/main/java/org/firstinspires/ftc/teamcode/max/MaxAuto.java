b6d550eaa1a2abe6d39228425b3b8849322bfc10
Max613Code <bzhouadult@gmail.com>
2022-08-03 11:29:00 -0700

    .............

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/max/MaxAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/max/MaxAuto.java
@@ -19,7 +19,7 @@ private Robot robot;
         initialize();
 
         robot.lifter.setPower(0.3); //Lifting up the arm for hub
-        sleep(365);
+        sleep(375);
         robot.lifter.setPower(-0.05);
 
         robot.drive(0.45); //Driving to hub carefully
@@ -68,7 +68,7 @@ private Robot robot;
         sleep(100);
         robot.turn(0);
         robot.drive(0.4);
-        sleep(1500);
+        sleep(2000);
 
 
     }

124df3b2214216e52b1074afb22a6e3e6d4a60b7
Clincot <courtneyg509@gmail.com>
2022-08-04 09:31:59 -0700

    add AlexAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/alex/AlexAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/alex/AlexAuto.java
@@ -33,7 +33,7 @@ public class AlexAuto extends LinearOpMode {
         sleep(800);
         robot.drive(0, 0);
         robot.drive(-0.35);
-        sleep(2500);
+        sleep(3000);
         robot.drive(0);
         robot.spinCarousel(0.4);
         sleep(4000);
